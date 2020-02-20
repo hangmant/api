a4be4d323797907f874b1f2692853e54d7523ce4
Dante Calderon <mail@dantecalderon.dev>
2020-02-20 01:28:28 -0500

    Limit word name to 20 characters

--- src/modules/words/words.model.ts
+++ src/modules/words/words.model.ts
@@ -1,15 +1,14 @@
 import { prop, Ref } from '@typegoose/typegoose'
-import { IsString, IsNotEmpty, IsMongoId } from 'class-validator'
+import { IsString, IsNotEmpty, IsMongoId, MaxLength, MinLength } from 'class-validator'
 import { Category } from '../categories/categories.model'
 
 export class Word {
   @IsString()
-  @IsNotEmpty()
-  @prop({ required: true })
+  @prop({ required: true, maxlength: 20 })
   name: string
 
   @IsMongoId()
   @IsNotEmpty()
-  @prop({ ref: 'Category' })
+  @prop({ ref: 'Category', required: true })
   category: Ref<Category>
 }
