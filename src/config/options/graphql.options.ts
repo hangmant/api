1907a7ecd43842e4854be543deb4c913d359438d
Dante Calderon <mail@dantecalderon.dev>
2020-07-23 01:06:53 -0500

    Move to correct folder

--- src/config/options/graphql.options.ts
+++ src/config/options/graphql.options.ts
@@ -3,7 +3,7 @@ import { GqlModuleOptions, GqlOptionsFactory } from '@nestjs/graphql'
 import { GraphQLObjectId } from 'graphql-objectid-scalar'
 import * as GraphQLJSON from 'graphql-type-json'
 import { join } from 'path'
-import { CountriesAPI } from './datasources/countries.datasource'
+import { CountriesAPI } from '../../datasources/countries.datasource'
 
 @Injectable()
 export class GraphqlOptions implements GqlOptionsFactory {
