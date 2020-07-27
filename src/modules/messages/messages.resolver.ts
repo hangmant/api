adb5cff232f0fb936f1b099d8b36f9ad975e7d88
Dante Calderon <mail@dantecalderon.dev>
2020-07-27 01:20:46 -0500

    Publish messageCreated

--- src/modules/messages/messages.resolver.ts
+++ src/modules/messages/messages.resolver.ts
@@ -2,7 +2,7 @@ import { NotFoundException, UseGuards } from '@nestjs/common'
 import { Args, ID, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql'
 import { PubSub } from 'apollo-server-fastify'
 import { of, throwError } from 'rxjs'
-import { concatMap } from 'rxjs/operators'
+import { concatMap, tap } from 'rxjs/operators'
 import { CurrentUser } from '../../decorators/currentUser.decorator'
 import { GqlAuthGuard } from '../../guards/gqlAuth.guard'
 import { MessageCreateInput } from './dto/message-create.input'
@@ -12,7 +12,6 @@ import { Message } from './models/message.model'
 
 const pubSub = new PubSub()
 
-@UseGuards(GqlAuthGuard)
 @Resolver(of => Message)
 export class MessagesResolver {
   constructor(private readonly messagesService: MessagesService) {}
@@ -27,16 +26,18 @@ export class MessagesResolver {
     )
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(returns => Message)
   createMessage(@CurrentUser() user, @Args('data') message: MessageCreateInput) {
     message.fromUser = user._id
 
-    pubSub.publish('messageCreated', {
-      text: message.text,
-      roomId: message.roomId,
-      fromUser: user._id
-    })
-    return this.messagesService.create(message)
+    return this.messagesService.create(message).pipe(
+      tap(messageCreated => {
+        pubSub.publish('messageCreated', {
+          messageCreated
+        })
+      })
+    )
   }
 
   @Subscription(returns => Message)
