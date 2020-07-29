79431855dac3ed2d0753aa4d744ff64c2d36d596
Dante Calderon <mail@dantecalderon.dev>
2020-07-28 19:59:12 -0500

    Revert "Use default host"
    This reverts commit 64e7b78ae94fed04a4c9ec8af56cf319d61ecc95.

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(config.port)
+  await app.listen(config.port, config.host)
 
   if (config.env === 'dev' || true) {
     const logger = app.select(LoggerModule).get(LoggerService)
