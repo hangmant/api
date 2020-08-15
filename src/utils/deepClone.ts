52bb69dbd37d0137f0e6f7b790b786e61aa3f10c
Dante Calderon <mail@dantecalderon.dev>
2020-08-14 23:39:43 -0500

    Add tests for utils

--- src/utils/deepClone.ts
+++ src/utils/deepClone.ts
@@ -1,3 +1,3 @@
-export function deepClone(value) {
+export function deepClone<T extends object>(value: T): T {
   return JSON.parse(JSON.stringify(value))
 }
--- src/utils/dlSort.ts
+++ src/utils/dlSort.ts
@@ -1,7 +1,14 @@
 import keyBy = require('lodash.keyby')
-import get = require('lodash.get')
 
-export function dlSort(keys: readonly string[], listValues: any[], fieldKey: string = '_id') {
-  const listByKey = keyBy(listValues, fieldKey)
-  return keys.map(_id => get(listByKey, _id, null))
+/**
+ * Create new array with "values for keys" in the same order, if the
+ * key doesn't exist in values, returns null for that index.
+ * *It's useful to load data for graphql dataloaders
+ * @param keys keys to get values
+ * @param values values where the keys will be searched
+ * @param {string} [fieldKey="_id"] field name in a value to compare with a key
+ */
+export function dlSort<T extends object>(keys: readonly string[], values: T[], fieldKey: string = '_id'): T[] {
+  const valuesMap = keyBy(values, fieldKey)
+  return keys.map(key => valuesMap?.[key] ?? null)
 }
