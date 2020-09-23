299ddb4a01615291464b1ab084100fd951e162a8
Dante Calderón <mail@dantecalderon.dev>
2020-09-22 21:18:08 -0500

    feat: Support partial updates for category (#33)

--- src/modules/categories/dto/category-update.input.ts
+++ src/modules/categories/dto/category-update.input.ts
@@ -1,16 +1,19 @@
-import { IsHexColor, IsString } from 'class-validator'
+import { IsHexColor, IsString, IsOptional } from 'class-validator'
 import { InputType, Field } from '@nestjs/graphql'
 
 @InputType()
 export class CategoryUpdateInput {
+  @IsOptional()
   @Field({ nullable: true })
   name?: string
 
+  @IsOptional()
   @IsHexColor()
   @IsString()
   @Field({ nullable: true })
   color?: string
 
-  @Field()
+  @IsOptional()
+  @Field({ nullable: true })
   description?: string
 }
