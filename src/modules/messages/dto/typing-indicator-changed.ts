6107fea5abebd0fc02b307d93739ce0e681687da
Dante Calderón <mail@dantecalderon.dev>
2020-09-27 00:22:33 -0500

    feat: Implement users typing functionality (#35)

---
+++ src/modules/messages/dto/typing-indicator-changed.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from '@nestjs/graphql'
+
+@ObjectType()
+class UserTypingIndicatorChanged {
+  @Field()
+  _id: string
+
+  @Field()
+  name: string
+}
+
+@ObjectType()
+export class TypingIndicatorChanged {
+  @Field(type => UserTypingIndicatorChanged)
+  user: UserTypingIndicatorChanged
+
+  @Field()
+  isTyping: boolean
+}
